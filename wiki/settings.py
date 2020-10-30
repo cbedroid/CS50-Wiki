a9432eb2ff1d964606bb0ae20041da2a9597d2f8
cbedroid <cbedroid1614@gmail.com>
2020-10-29 20:45:43 -0400

    Debugging heroku staticfiles error

--- wiki/settings.py
+++ wiki/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -130,8 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = "/static/"
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 mimetypes.add_type("text/css", ".css", True)
 django_heroku.settings(locals())
